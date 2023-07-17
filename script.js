0687d38803e4d1623c2d260fffeb62aeca7601f5
Sourav Paul <souravpaul608@gmail.com>
2023-07-17 17:26:11 +0530

    5th commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let searchBtn=document.querySelector('#search-btn');
 let searchBar=document.querySelector('.search-bar-container');
+let loginBtn=document.querySelector('#login-btn');
+let loginForm=document.querySelector('.login-form-container');
+let loginClose=document.querySelector('#form-close');
 
 window.onscroll=()=>{
     searchBtn.classList.remove('fa-times');
@@ -9,4 +12,11 @@ window.onscroll=()=>{
 searchBtn.addEventListener('click',()=>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+});
+
+loginBtn.addEventListener('click',()=>{
+    loginForm.classList.add('active');
+});
+loginClose.addEventListener('click',()=>{
+    loginForm.classList.remove('active');
 });
